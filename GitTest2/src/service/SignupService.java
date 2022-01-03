01bbea11710b38b665d255810d8782af2694107f
smhrd <smhrd@DESKTOP-6I43JLN>
2022-01-03 17:10:22 +0900

    asdf

--- GitTest2/src/service/SignupService.java
+++ GitTest2/src/service/SignupService.java
@@ -26,33 +26,45 @@ public class SignupService extends HttpServlet implements command {
 		
 		String id= request.getParameter("id");
 		String pw= request.getParameter("pw");
+		String pw2= request.getParameter("pw2");
 		String nick= request.getParameter("nickname");
 		String name= request.getParameter("name");
 		String birth= request.getParameter("birth");
 		String sex= request.getParameter("gender");
 		String phone= request.getParameter("phone");
-		System.out.println("회원서블릿성공");
-		UserVO uservo = new UserVO(id, pw, nick, name, birth, sex, phone);
-		UserDAO dao = new UserDAO();
-		dao.signUp(uservo);
-		
-		String nextpage="";
-		if (dao.signUp(uservo)>0) {
-			nextpage = "main.jsp";
-			System.out.println("회원가입성공");
-			
-		}
-		else {
-			System.out.println("회원가입실패");
-		}
-		
 		response.setContentType("text/html; charset=UTF-8");
+	
+		System.out.println("회원서블릿성공");
+	
 		PrintWriter out = response.getWriter();
-		out.print("<script>");
-		out.print("alert('회원가입 성공');");
-		out.print("location.href='main.jsp';");
-		out.print("</script>");
+		if(pw.equals(pw2)) {
+			UserVO uservo = new UserVO(id, pw, nick, name, birth, sex, phone);
+			UserDAO dao = new UserDAO();
+			dao.signUp(uservo);
+			
+			
+			if (dao.signUp(uservo)>0) {
+				
+				System.out.println("회원가입성공");
+				
+			}
+			else {
+				System.out.println("회원가입실패");
+			}
+			
+			out.print("<script>");
+			out.print("alert('회원가입 성공');");
+			out.print("location.href='main.jsp';");
+			out.print("</script>");
+			
+		}else {
+			out.print("<script>");
+			out.print("alert('비밀번호가 다릅니다');");
+			out.print("location.href='signUp.jsp';");
+			out.print("</script>");
+		}
 		
+	
 		return null;
 	}
 
